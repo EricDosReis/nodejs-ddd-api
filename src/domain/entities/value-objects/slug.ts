91a4dc4351a3e6a71127a0da8824bfc76617908c
EricDosReis <ericdosreis.personal@gmail.com>
2024-04-26 15:27:15 -0300

    feat: create base entities

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,24 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Receives a string and normalize it as a slug
+   *
+   * Example: "An example title" => "an-example-title"
+   */
+  static createFromText(text: string) {
+    const normalizedText = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/-$/, "");
+
+    return new Slug(normalizedText);
+  }
+}
