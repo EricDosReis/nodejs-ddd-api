d14074fe7e517ca366fcf3247d9151c38d6454d3
EricDosReis <ericdosreis.personal@gmail.com>
2024-04-30 15:06:44 -0300

    test: setup faker to generate random data in tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,26 @@
+import { faker } from '@faker-js/faker';
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import type { QuestionProps } from '@/domain/forum/enterprise/entities/question';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 
-export function makeQuestion(override?: Partial<QuestionProps>) {
-  const question = Question.create({
-    title: 'Question title',
-    slug: Slug.createFromText('Question title'),
-    authorId: new UniqueEntityID(),
-    content: 'Question content',
-    ...override,
-  });
+export function makeQuestion(
+  override?: Partial<QuestionProps>,
+  id?: UniqueEntityID,
+) {
+  const title = faker.lorem.sentence();
+
+  const question = Question.create(
+    {
+      title,
+      slug: Slug.createFromText(title),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }
